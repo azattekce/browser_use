dbd746553a06767a2d35ac5c2055fd60ecd9c8c8
Azat Tekçe <Azat.Tekce@opet.com.tr>
2025-11-17 15:40:52 +0300

    feat: Add Procfile for Gunicorn web server and ensure proper newline in requirements.txt

--- run.py
+++ run.py
@@ -3,6 +3,10 @@ import os
 
 app = create_app()
 
+@app.route("/test")
+def home():
+    return "Koyeb üzerinden Flask yayında!"
+
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5005))
     debug = os.environ.get('FLASK_ENV', 'production') != 'production'
